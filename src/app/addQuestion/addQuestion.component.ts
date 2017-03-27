ef8a8fbf2480b7007a15a67a8719b1f246db6d94
peterjza@gmail.com <peterjza@gmail.com>
2017-03-27 19:11:24 +0200

    radio buttons working

--- src/app/addQuestion/addQuestion.component.ts
+++ src/app/addQuestion/addQuestion.component.ts
@@ -13,6 +13,7 @@ import { Customer } from './customer.interface';
 export class AddQuestionComponent  implements OnInit {
 
   public localState: any;
+  public correctAnswerIdx: number;
   public myForm: FormGroup; // our form model
   constructor(
     public route: ActivatedRoute,
@@ -20,30 +21,34 @@ export class AddQuestionComponent  implements OnInit {
   ) {}
 
     public ngOnInit() {
-    // we will initialize our form here
+    this.correctAnswerIdx = 0;
     this.myForm = this._fb.group({
             question: ['', [Validators.required, Validators.minLength(5)]],
+            correctAnswer: ['', [Validators.required]],
             answers: this._fb.array([
                 this.initAnswer(),
                 this.initAnswer(),
             ])
         });
+
     }
     public initAnswer() {
         // initialize our address
         return this._fb.group({
-            answer: ['', Validators.required],
-            correct: ['']
-        });
+            answer: ['', Validators.required]
+        }); 
     }
     public addAnswer() {
-
       const control = <FormArray>this.myForm.controls['answers'];
       control.push(this.initAnswer());
     }
     public removeAnswer(i: number) {
       const control = <FormArray>this.myForm.controls['answers'];
       control.removeAt(i);
+    }
+    public setCorrectAnswer(i) {
+      this.correctAnswerIdx = i;
+
     }
     public save(model: Customer) {
         // call API to save customer
